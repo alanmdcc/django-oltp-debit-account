06849dd24fea9fec6300fb80a8d5d1779258437a
Alan Martin del Campo Cervantes <Alan.MartinDelCampo.C@ibm.com>
2023-08-23 20:41:27 -0600

    add models to admin

--- debit_app/admin.py
+++ debit_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from debit_app.models import User, Account, Card
+
+admin.site.register(User)
+admin.site.register(Account)
+admin.site.register(Card)
