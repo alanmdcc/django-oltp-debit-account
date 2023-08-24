5c8e8ebc7fecd23eb7431952d85583dc209f0335
Alan Martin del Campo Cervantes <Alan.MartinDelCampo.C@ibm.com>
2023-08-23 20:33:26 -0600

    create models

--- debit_app/models.py
+++ debit_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    age = models.IntegerField
+    name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __repr__(self):
+        return self.name
+
+class Account(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=19, decimal_places=2)
+    open_date = models.DateField
+
+    def __repr__(self):
+        return f"Account from User: {self.user_id}"
+
+class Card(models.Model):
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    cvv = models.CharField(max_length=3)
+
+    def  __repr__(self):
+        return f"Card {self.name} from Account: {self.account_id}"
