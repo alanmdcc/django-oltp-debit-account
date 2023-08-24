cfdb3edba9c3c4dfa4fe44ac92af6e74e93a03f7
Alan Martin del Campo Cervantes <Alan.MartinDelCampo.C@ibm.com>
2023-08-24 13:32:59 -0600

    add constraint of positive balance

--- debit_app/models.py
+++ debit_app/models.py
@@ -1,3 +1,6 @@
+from _decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class User(models.Model):
@@ -9,7 +12,7 @@ class User(models.Model):
 
 class Account(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    balance = models.DecimalField(max_digits=19, decimal_places=2)
+    balance = models.DecimalField(max_digits=19, decimal_places=2, validators=[MinValueValidator(Decimal('0.00'))])
     open_date = models.DateField()
 
     def __str__(self):
--- debit_app/views.py
+++ debit_app/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -58,16 +59,22 @@ class UserDetailView(APIView):
 
 class AccountView(APIView):
 
+    def validatePositiveBalance(self, balance):
+        if float(balance) < 0:
+            raise ParseError(detail="Balance must be positive")
+        return balance
+
     def get(self, _):
         accounts = Account.objects.all()
         serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        new_balance = self.validatePositiveBalance(request.data['balance'])
         try:
             account = Account.objects.create(
                 user_id=User.objects.get(id=request.data['user_id']),
-                balance=request.data['balance'],
+                balance=new_balance,
                 open_date=request.data['open_date']
             )
         except User.DoesNotExist:
@@ -77,6 +84,11 @@ class AccountView(APIView):
 
 
 class AccountDetailView(APIView):
+
+    def validatePositiveBalance(self, balance):
+        if (float(balance) < 0):
+            raise ParseError(detail="Balance must be positive")
+        return balance
 
     def get_account(self, id):
         try:
@@ -96,9 +108,10 @@ class AccountDetailView(APIView):
         return Response(serializer.data)
 
     def put(self, request, id):
+        new_balance = self.validatePositiveBalance(request.data['balance'])
         account = self.get_account(id=id)
         account.user_id = self.get_user(id=request.data['user_id'])
-        account.balance = request.data['balance']
+        account.balance = new_balance
         account.open_date = request.data['open_date']
         account.save()
         serializer = AccountSerializer(account, many=False)
